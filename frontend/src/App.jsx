78538b2314c93a6fc4308d7d0781e8baf0e5cac8
Vipun Sanjana Jayathunga <vipunsanjana34@gmail.com>
2025-07-24 17:03:22 +0530

    nooo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,18 @@ import './App.css';
 function App() {
   const [message, setMessage] = useState('Loading...');
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => setMessage(err.message || 'Error fetching data'));;
-  }, []);
+  // useEffect(() => {
+  //   fetch('/api/hello')
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message))
+  //     .catch((err) => setMessage(err.message || 'Error fetching data'));;
+  // }, []);
 
   return (
     <div className="container">
       <div className="card">
         <h1 className="title">🚀 MERN App</h1>
-        <p className="message">{message}</p>
+        <p className="message">"hhhhhhhhhhhhhhhhh"</p>
       </div>
     </div>
   );
